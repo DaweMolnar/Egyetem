2d6bdc89a64343470035a66f682a034b35d92629
Decesso <dawemolnar@gmail.com>
2015-12-15 14:16:08 +0100

    add parameter for random lines number

--- numanal/random_tetel.py
+++ numanal/random_tetel.py
@@ -3,8 +3,9 @@ import random
 import sys, getopt
 def main(argv):
     inputfile = ''
+    numtimes = 0
     try:
-        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+        opts, args = getopt.getopt(argv,"hi:n:",["ifile=","num="])
     except getopt.GetoptError:
       print 'random_tetel.py -i <inputfile>'
       sys.exit(2)
@@ -14,8 +15,11 @@ def main(argv):
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
-    line = random.choice(open(inputfile).readlines())
-    print line
+        elif opt in ("-n", "--num"):
+            numtimes = int(arg)
+    for x in xrange(numtimes):
+        line = random.choice(open(inputfile).readlines())
+        print line
 
 if __name__ == "__main__":
        main(sys.argv[1:])
